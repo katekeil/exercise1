98449ab0b4caf8f0eeaecf36ca7e89b9d6086a03
12th CCHS Kathryn A Keil <016006@SWCS.K12>
2019-01-17 13:35:46 -0500

    Initial commit

---
+++ src/com/kate/Week1/Main.java
@@ -0,0 +1,21 @@
+package com.kate.Week1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String city="Columbus";
+	int zip=43215;
+	int highTemp[]={32,25,27,40,45};
+	float sum=0;
+	float average = 0;
+
+	for(int i=0; i<=4; i++) {
+		sum = sum + highTemp[i];
+	}
+
+	average = sum/5;
+
+		System.out.println("City: " + city + " Zip code: " + zip + " Average High Temperature: " + average);
+
+    }
+}
